534f8d9d84f601c65d47fb78a3250474a6a89fee
Phelippe Souza-Herod <psouzaherod6138@floridapoly.edu>
2020-09-20 13:53:44 -0400

    fixed a typo mistake when displaying operation of the file

--- user/getpidcounttest.c
+++ user/getpidcounttest.c
@@ -6,7 +6,7 @@ int main(void) {
 	
 	printf(0, "initial pid count: %d \n", getpidcount());
 
-	printf(0, "calling pid count 3 times \n");
+	printf(0, "calling getpid() 3 times \n");
 	
 	int i;
 	for( i = 0; i < 3; i++) {
@@ -15,7 +15,7 @@ int main(void) {
 		
 	printf(0, "current pid count: %d \n", getpidcount());
 	
-	printf(0, "calling pid count 2 more times \n");
+	printf(0, "calling getpid() 2 more times \n");
 
 	for(i = 0; i < 2; i++) {
 		printf(0, "pid of process: %d \n", getpid());
